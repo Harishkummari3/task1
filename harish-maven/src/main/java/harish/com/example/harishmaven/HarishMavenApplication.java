a1e622c5a07303cacdfb4586b09785f4173ff3d0
M1082798 <M1082798@MT-PF1WABZ4>
2022-12-20 19:29:19 +0530

    sending the files into github

---
+++ harish-maven/src/main/java/harish/com/example/harishmaven/HarishMavenApplication.java
@@ -0,0 +1,14 @@
+package harish.com.example.harishmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarishMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarishMavenApplication.class, args);
+		System.out.println("Hi, welcome to my world.... ! you will see nothing here.");
+	}
+
+}
---
+++ harish-maven/src/test/java/harish/com/example/harishmaven/HarishMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package harish.com.example.harishmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarishMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
